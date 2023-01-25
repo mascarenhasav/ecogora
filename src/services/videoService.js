596a5484710bd3f3c99c6aed732fa9858d6b3273
mascarenhasav <mascarenhasav@gmail.com>
2023-01-25 18:23:13 +0900

    supabase

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const PROJECT_URL = "https://dwrmdvgyhinnsisyeqas.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNtbWhyamtyeHJ3eHpsaXF3Z3R0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg1MTE4ODYsImV4cCI6MTk4NDA4Nzg4Nn0.iaKpCVdcmNBG5OHwMhSZ_NV8KDY3PgD1XYItdjuazMI";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR3cm1kdmd5aGlubnNpc3llcWFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg1MjQ1MzcsImV4cCI6MTk4NDEwMDUzN30.ljXNR4or1wR0z9sc77fDbioofYVoexnoWmQSYNQxQo0";
 const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 export function videoService() {
