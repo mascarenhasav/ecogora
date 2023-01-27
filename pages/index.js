eb9a65383b757cfbdfbb72a5b84383b43390ec43
mascarenhasav <mascarenhasav@gmail.com>
2023-01-27 19:37:36 +0900

    ae

--- pages/index.js
+++ pages/index.js
@@ -87,7 +87,7 @@ function Header() {
         <StyledHeader>
             <StyledBanner bg={config.bg} />
             <section className="user-info">
-                <img src={"https://github.com/mascarenhasav/ecogora/blob/268489b5b806796913f8474f1bfde07c44ff3423/images/logo/logo.png"} />
+                <img src={"https://raw.githubusercontent.com/mascarenhasav/ecogora/268489b5b806796913f8474f1bfde07c44ff3423/images/logo/logo.png"} />
                 <div>
                     <h2>
                         {config.name}
